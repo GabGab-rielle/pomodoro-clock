be0e76868b387703afeb9f9c5dc5476544e25a04
Gabrielle Popoola <44483818+GabGab-rielle@users.noreply.github.com>
2020-10-07 22:29:34 +0100

    made time left synchonous with session time

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,6 +1,6 @@
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // to setup , call setup function and pass moment package
 momentDurationFormatSetup(moment);
@@ -8,6 +8,15 @@ momentDurationFormatSetup(moment);
 const TimeLeft = ({ sessionLength }) => {
   // initialise time left state to be session length
   const [timeLeft, setTimeLeft] = useState(sessionLength);
+
+  // change timeLeft whenever sessionLength changes
+  // function which takes in a callback that is called whenever a variable that you are
+  // listening on changes.
+  useEffect(() => {
+    setTimeLeft(sessionLength);
+
+    // array is dependency list with all vairables we're listening on
+  }, [sessionLength]);
 
   // set format to MM:SS
   const formattedTimeLeft = moment.duration(timeLeft, "s").format("mm:ss");
